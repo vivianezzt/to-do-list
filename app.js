df3dce1774d107003d40547ccd1638ef5465c45e
Viviane <vivianesilvarc@outlook.com>
2023-04-16 20:02:39 -0300

    Enviando dados para um templates EJS

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const checklistRouter = require('./src/routes/checklist')
-const app = express();
+const path = require('path');
+
+const checklistRouter = require('./src/routes/checklist');
+const rootRouter = require('./src/routes/index');
 require('./config/database');
 
+
+
+const app = express();
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
+app.use('/', rootRouter);
 app.use('/checklists', checklistRouter);
+
 
 app.listen(3000, () => {
     console.log('Servidor iniciado!');
