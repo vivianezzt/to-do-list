b8c78e12428186f6c939b3f97fd63fac5561e398
Viviane <vivianesilvarc@outlook.com>
2023-04-16 18:04:08 -0300

    finalizando o modulo nodejs

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const checklistRouter = require('./src/routes/checklist')
+const app = express();
+
+app.use(express.json());
+
+app.use('/checklists', checklistRouter);
+
+app.listen(3000, () => {
+    console.log('Servidor iniciado!');
+})
---
+++ src/routes/checklist.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    console.log('Olá!');
+    res.send();
+})
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.status(200).send(req.body);
+})
+
+router.get('/:id', (req, res) => {
+    console.log(req.params);
+    res.send(`ID: ${req.params.id}`);
+})
+
+router.put('/:id', (req, res) => {
+    console.log(req.params);
+    res.send(`PUT ID: ${req.params.id}`);
+})
+
+router.delete('/:id', (req, res) => {
+    console.log(req.params);
+    res.send(`DELETE: ${req.params.id}`);
+})
+module.exports = router;
